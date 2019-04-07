5d75f6bf3654660edf2f742613baef40931b999f
md ashraf Uddin <mdtanjil01753511918@gmail.com>
2019-04-07 10:09:17 +0600

    Passoprt insstall

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+           // $tabke->string('api_token'); // specify the length also
             $table->rememberToken();
             $table->timestamps();
         });
